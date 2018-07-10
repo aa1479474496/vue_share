e149c62c0fb1cf1212cf17229e8d20cc9da07a97
zhashaonan <1479474496@qq.com>
2018-07-10 16:47:41 +0800

    update

---
+++ src/components/share/Demo1.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+
+<script>
+  export default {
+    name: 'Demo1',
+    data() {
+      return {
+       title: 'Demo1' 
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Demo1 from '@/components/share/Demo1'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/demo1',
+      name: 'Demo1',
+      component: Demo1
     }
   ]
 })
